9b5441c7523111dda996f744cf9add5cd42b6d33
arya-agridoctor <arya.kabir.1362@gmail.com>
2026-09-20 01:51:28 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'home_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const AryaApp());
+}
+
+class AryaApp extends StatelessWidget {
+  const AryaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ARYA AgriDoctor',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.green,
+        scaffoldBackgroundColor: const Color(0xFFF5F8F5),
+      ),
+      home: const AryaHomePage(),
+    );
+  }
 }
